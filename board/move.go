94941a63d2982d81faaae9f43789d74908f7fd53
Casey Allred <sbditto85@gmail.com>
2013-10-17 16:19:46 -0600

    added pawn moves, not verified

--- board/move.go
+++ board/move.go
@@ -10,15 +10,15 @@ func colorMatch(piece, color string) bool {
 
 func (board Board) GetAllMoves(color string) []Move {
 	moves := []Move{}
-	
+
 	for i,piece := range board {
 		if !colorMatch(piece, color) {
 			continue
 		}
-		
+
 		switch piece {
 		case "WP", "BP":
-			moves = append(moves, board.GetPawnMoves(i)...)
+			moves = append(moves, board.GetPawnMoves(i,color)...)
 		case "WR", "BR": // rooks
 		case "WH", "BH": // horses/knights
 			moves = append(moves, board.GetHorseMoves(i)...)
@@ -40,13 +40,75 @@ func (board Board) GetAllMoves(color string) []Move {
 // 48 49 50 51 52 53 54 55
 // 56 57 58 59 60 61 62 63
 
-func (board Board) GetPawnMoves(i int) []Move {
-	return []Move{}
+func (board Board) GetPawnMoves(i int, color string) []Move {
+	moves := []Move{}
+
+	if color == "white" {
+		if i > 47 && i < 56 {
+			if board[i-8] == "00" {
+				moves = append(moves, Move{i,i-8})
+			}
+			if board[i-8] == "00" && board[i-16] == "00" {
+				moves = append(moves, Move{i,i-16})
+			}
+		} else {
+			if i-8 >= 0 && board[i-8] == "00" {
+				moves = append(moves, Move{i,i-8})
+			}
+		}
+		switch i {
+		case 0: case 8: case 16: case 24:
+		case 32: case 40: case 48: case 56:
+		default:
+			if i-9 >= 0 && board[i-9] != "00" && !colorMatch(board[i-9],color) {
+				moves = append(moves, Move{i,i-9});
+			}
+		}
+		switch i {
+		case 7: case 15: case 23: case 31:
+		case 39: case 47: case 55: case 63:
+		default:
+			if i-7 >= 0 && board[i-7] != "00" && !colorMatch(board[i-7],color) {
+				moves = append(moves, Move{i,i-7});
+			}
+		}
+	} else { //black
+		if i > 7 && i < 16 {
+			if board[i+8] == "00" {
+				moves = append(moves, Move{i,i+8})
+			}
+			if board[i+8] == "00" && board[i+16] == "00" {
+				moves = append(moves, Move{i,i+16})
+			}
+		} else {
+			if i+8 >= 0 && board[i+8] == "00" {
+				moves = append(moves, Move{i,i+8})
+			}
+		}
+		switch i {
+		case 7: case 15: case 23: case 31:
+		case 39: case 47: case 55: case 63:
+		default:
+			if i+9 <= 63 && board[i+9] != "00" && !colorMatch(board[i+9],color) {
+				moves = append(moves, Move{i,i+9});
+			}
+		}
+		switch i {
+		case 0: case 8: case 16: case 24:
+		case 32: case 40: case 48: case 56:
+		default:
+			if i+7 <= 63 && board[i+7] != "00" && !colorMatch(board[i+7],color) {
+				moves = append(moves, Move{i,i+7});
+			}
+		}
+	}
+
+	return moves
 }
 
 func (board Board) GetHorseMoves(i int) []Move {
 	moves := []Move{}
-	
+
 	if i > 15 { // can move up 2
 		if i % 8 < 7 { // can move right 1
 			moves = append(moves, Move{i, i-15})
