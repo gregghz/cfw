ebc5190570a4f4aae83096dc23277f13b0046241
Gregg Hernandez <greggory.hz@gmail.com>
2013-10-20 14:59:42 -0600

    ability to run multiple games at once

--- main.go
+++ main.go
@@ -12,30 +12,36 @@ import (
 	"os"
 )
 
-func main() {
-	white := flag.String("white", "ais/random/random", "the path to white's executable.")
-	black := flag.String("black", "ais/random/random", "the path to black's executable.")
+type Result string
 
+const (
+	WhiteWin = "WhiteWin"
+	BlackWin = "BlackWin"
+	Stalemate = "Stalemate"
+
+	WhiteIllegalMove = "WhiteIllegalMove"
+	BlackIllegalMove = "BlackIllegalMove"
+
+	WhiteCrash = "WhiteCrash"
+	BlackCrash = "BlackCrash"
+)
+
+func RunGame(white, black string) Result {
 	color := "white"
 	brd := board.NewStartingBoard()
-
-	flag.Parse()
-
-	fmt.Printf("white: %s\n", *white)
-	fmt.Printf("black: %s\n", *black)
 
 	for {
 		var cmd *exec.Cmd
 		var stdin io.Reader
 
+		legalMoves := brd.GetAllMoves(color)
+
 		if color == "white" {
-			fmt.Println("\nwhite move . . . ")
-			cmd = exec.Command(*white)
+			cmd = exec.Command(white)
 			stdin = strings.NewReader("white " + brd.String() + "\n")
 			color = "black"
 		} else {
-			fmt.Println("\nblack move . . . ")
-			cmd = exec.Command(*black)
+			cmd = exec.Command(black)
 			stdin = strings.NewReader("black " + brd.String() + "\n")
 			color = "white"
 		}
@@ -65,23 +71,76 @@ func main() {
 			log.Fatal(err)
 		}
 
-		// @TODO
-		// verify the move is valid
-		// get all legal moves for moving color
-		// look up move in this list of legal moves
+		// verify valid move
+		legal := false
+		for _, lm := range legalMoves {
+			if move.Src == lm.Src && move.Dest == lm.Dest {
+				legal = true
+			}
+		}
+
+		if !legal {
+			if color == "white" {
+				return BlackIllegalMove
+			} else {
+				return WhiteIllegalMove
+			}
+		}
 
 		brd = brd.MakeMove(move)
 		fmt.Println(brd.Display())
 
 		if brd.Checkmated(color) {
-			fmt.Println("checkmate")
-			break
+			if color == "white" {
+				return BlackWin
+			} else {
+				return WhiteWin
+			}
 		}
 
 		// check for stalemate
 		if brd.Stalemate(color) {
-			fmt.Println("stalemate")
-			break
+			return Stalemate
 		}
+	}
+}
+
+func PrintResults(results map[Result]int) {
+	fmt.Printf("=== Results ===\n")
+	fmt.Printf("White win: %d\n", results[WhiteWin])
+	fmt.Printf("Black win: %d\n", results[BlackWin])
+	fmt.Printf("Stalemate: %d\n\n", results[Stalemate])
+
+	fmt.Printf("White Illegal: %d\n", results[WhiteIllegalMove])
+	fmt.Printf("Black Illegal: %d\n", results[BlackIllegalMove])
+	fmt.Printf("White Crash: %d\n", results[WhiteCrash])
+	fmt.Printf("Black Crash: %d\n\n", results[BlackCrash])
+}
+
+func main() {
+	white := flag.String("white", "ais/random/random", "the path to white's executable.")
+	black := flag.String("black", "ais/random/random", "the path to black's executable.")
+	n := flag.Int("games", 1, "how many games to run.")
+
+	flag.Parse()
+
+	fmt.Printf("white: %s\n", *white)
+	fmt.Printf("black: %s\n", *black)
+
+	results := map[Result]int{
+		WhiteWin: 0,
+		BlackWin: 0,
+		Stalemate: 0,
+		WhiteIllegalMove: 0,
+		BlackIllegalMove: 0,
+		WhiteCrash: 0,
+		BlackCrash: 0,
+	}
+
+	for i := 0; i < *n; i++ {
+		fmt.Printf("game #%d\n", i+1)
+		results[RunGame(*white, *black)]++
+
+		PrintResults(results)
 	}
 }
