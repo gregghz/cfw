50cd513ce97617b01f8f27c120be1dbae732a225
Casey Allred <sbditto85@gmail.com>
2013-10-22 23:52:24 -0600

    idea for interface for different communication methods text/json/xml/whatever

---
+++ communicator/jsoncommunicator.go
@@ -0,0 +1,28 @@
+package communicator
+
+import (
+	"github.com/greggoryhz/cfw/board"
+	"io"
+	"strings"
+	"fmt"
+)
+
+type JsonCommunicator struct{}
+
+func (j *JsonCommunicator) GenerateRequest(isWhite bool,brd board.Board) io.Reader {
+	var stdin io.Reader
+	if isWhite {
+		stdin = strings.NewReader("white " + brd.String() + "\n")
+	} else {
+		stdin = strings.NewReader("black " + brd.String() + "\n")
+	}
+	return stdin
+}
+
+func (j *JsonCommunicator) ProcessResponse(out io.Reader) board.Move {
+	var move board.Move
+
+	fmt.Fscanf(out, "%d %d", &move.Src, &move.Dest)
+
+	return move
+}
