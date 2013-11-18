d7aae67da7cf63b9669fed6b0684a7b6431c58e1
Casey Allred <sbditto85@gmail.com>
2013-11-17 22:38:59 -0700

    actually 'returning' numbers

--- ais/haskell/lame.hs
+++ ais/haskell/lame.hs
@@ -1,9 +1,12 @@
 
 --pickMove x y = x ++ " ::: " ++ (foldr (\x acc -> x ++ " | " ++ acc) [] $ words y)
 
-pickMove x y = "49 41"
+-- sample input
+-- white BR BH BB BQ BK BB BH BR BP BP BP BP BP BP BP BP 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 WP WP WP WP WP WP WP WP WR WH WB WQ WK WB WH WR
+
+pickMove x y = "49 41\n"
 
 main = do
 	x <- getLine
 	let (y,z) = span (/=' ') x
-	print $ pickMove y z
+	putStr $ pickMove y z
